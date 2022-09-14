653e8537bba7a772e17c45bd0865797f685a8497
Shrutarshi Ray <91513676+shrutarshi2002@users.noreply.github.com>
2022-09-14 22:35:23 +0530

    add me

---
+++ tutorial/day2/index.js
@@ -0,0 +1,12 @@
+const arr = [2,3,4,5,6,7];
+function ray(){
+    alert("hello");
+}
+
+ray();
+
+document.write("hello world");
+
+arr.forEach(element => {
+    console.log(element);
+})
---
+++ tutorial/day3/index.js
@@ -0,0 +1,17 @@
+//document.querySelector("h1").style.color = "purple";
+function add(){
+    var a, b, sum;
+    a = Number(document.getElementById("first").value);
+    b = Number(document.getElementById("second").value);
+    sum = a + b;
+    alert(sum);
+    document.getElementById("add1").innerHTML = sum;
+}
+
+
+
+
+
+// let a = Number(prompt("enter number 1:"));
+// let b = Number(prompt("enter ur number2: "));
+// alert(a + b);
