556b466741457906d44956610fdd715ea85d4751
Shrutarshi Ray <91513676+shrutarshi2002@users.noreply.github.com>
2022-09-18 09:58:30 +0530

    add me

---
+++ tutorial/day4/index.js
@@ -0,0 +1,12 @@
+// const car = ["BMW","ford","nano"];
+
+// car.push("tata");
+// document.getElementById("demo").innerHTML = car;
+
+function myfunction(){
+    const ele = document.createElement("li");
+    ele.innerText = "bmw";
+    
+    document.getElementById("mylist").append(ele);
+
+}
